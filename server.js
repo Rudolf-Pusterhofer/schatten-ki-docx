841e7e919cfcacc971e3c417cdb4769efd8b858c
Rudolf-Pusterhofer <rudolfpusterhofer@gmail.com>
2026-05-28 11:18:11 +0200

    Update server.js
    Finale Ausgabe - 28-5-26

--- server.js
+++ server.js
@@ -276,7 +276,6 @@ app.post('/generate', async function(req,res){
           }),
           SP(60),
           ...berichtAbsaetze(ki_bericht),
-          new Paragraph({children:[new PageBreak()]}),
           SP(200),
           P('Eisenstadt, '+datum,{size:20,color:TEXT_GRAU,after:160}),
           new Paragraph({spacing:{before:0,after:80},children:[new TextRun({text:anrede+' '+nachname+',',font:'Arial',size:23,color:TEXT})]}),
